05fab2f193cefa41940d3c0eafc97455ed2f622f
Abhijit Das <abhijitadadas@gmail.com>
2023-04-04 01:35:39 +0530

    final commit

---
+++ client/src/App.js
@@ -0,0 +1,123 @@
+import "./App.css";
+import { useEffect } from "react";
+const { ethers } = require("ethers");
+function App() {
+  const walletaddress="0xC4C6E70C8a0b22C2884A03C2daCd6d88B1413a51"; //contract wallet
+
+  useEffect(() => {
+   
+
+
+    const walletabi=[
+      {
+        "inputs": [],
+        "name": "sendEthContract",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_user",
+            "type": "address"
+          }
+        ],
+        "name": "sendEthUser",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "_num",
+            "type": "uint256"
+          }
+        ],
+        "name": "setValue",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_address",
+            "type": "address"
+          }
+        ],
+        "name": "accountBalance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "contractBalance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "getValue",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
+    
+    const writeContract = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(walletaddress, walletabi, signer);
+      // await contract.setValue(2);
+      // await contract.sendEthContract({ value: ethers.utils.parseEther("0.1") }) ;
+
+      await contract.sendEthUser("0x46FC374d89D8FAb2dcbBACbDbD9Eb620D1A2AA8D", {
+        value: ethers.utils.parseEther("0.3"),
+      });
+
+    };
+    writeContract();
+  }, []);
+  return <div className="App"></div>;
+}
+
+export default App;
